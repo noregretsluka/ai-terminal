5b86df092d1a4d4187f5f38a780c869a6f8e55fc
BuildTools <unconfigured@null.spigotmc.org>
2025-07-19 15:17:56 -0500

    Style: Added VT323 font and updated Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        terminal: ['"VT323"', 'monospace'],
+      },
+    },
   },
   plugins: [],
 }
